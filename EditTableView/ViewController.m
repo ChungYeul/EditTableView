01b2a64d272410fc63ab55b7240a58d65c4016f5
ChungYeul In <sldk86@naver.com>
2014-01-06 18:11:23 +0900

    p184 3.4.11.실습 추가 하였습니다.
    p184 3.4.11.실습 추가 하였습니다.

--- EditTableView/ViewController.m
+++ EditTableView/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()<UITableViewDataSource, UITabBarDelegate> {
+    NSMutableArray *data;
 }
 @property (weak, nonatomic) IBOutlet UITableView *table;
 
@@ -32,30 +33,55 @@
     }
 }
 
+//
+- (BOOL)alertViewShouldEnableFirstOtherButton:(UIAlertView *)alertView {
+    // 입력 문자열 길이가 2 보다 클때만 추가 가능
+    NSString *inputStr = [alertView textFieldAtIndex:0].text;
+    return [inputStr length] > 2;
+}
+//
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    // 데이터 추가 Core Logic
+    [data addObject:[alertView textFieldAtIndex:0].text];
+    
+    // 테이블 셀 추가 UI Logic
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:([data count] - 1) inSection:0];
+    NSArray *row = [NSArray arrayWithObject:indexPath];
+    [self.table insertRowsAtIndexPaths:row withRowAnimation:UITableViewRowAnimationAutomatic];
+}
 // 삭제/추가 작업 - 로그로 확인
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (UITableViewCellEditingStyleDelete == editingStyle) {
-        NSLog(@"%d 번째 삭제", (int)indexPath.row);
+//        NSLog(@"%d 번째 삭제", (int)indexPath.row);
+        // 데이터 삭제
+        [data removeObjectAtIndex:indexPath.row];
+        
+        //테이블 셀 삭제
+        NSArray *rowList = [NSArray arrayWithObject:indexPath];
+        [tableView deleteRowsAtIndexPaths:rowList withRowAnimation:UITableViewRowAnimationAutomatic];
     }
     else {
-        NSLog(@"%d 번째 추가", (int)indexPath.row);
+//        NSLog(@"%d 번째 추가", (int)indexPath.row);
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"추가" message:nil delegate:self cancelButtonTitle:@"취소" otherButtonTitles:@"확인", nil];
+        alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+        [alert show];
     }
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 10;
+    return [data count];
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     // Dynamic Prototypes 방식 사용
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CELL_ID" forIndexPath:indexPath];
-    cell.textLabel.text = [NSString stringWithFormat:@"Cell %d", (int)indexPath.row];
+    cell.textLabel.text = data[indexPath.row];
     return cell;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    data = [[NSMutableArray alloc] initWithObjects:@"Item0", @"Item1", @"Item2", @"Item3", @"Item4", @"Item5", @"Item6", @"Item7", nil];
 }
 
 - (void)didReceiveMemoryWarning
