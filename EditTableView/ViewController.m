37c29022ee6e66b87caafbd95982e51da6d03817
ChungYeul In <sldk86@naver.com>
2014-01-06 17:44:08 +0900

    p182 3.4.10. 실습 하였습니다.
    p182 3.4.10. 실습 하였습니다.

--- EditTableView/ViewController.m
+++ EditTableView/ViewController.m
@@ -8,11 +8,49 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource, UITabBarDelegate> {
+}
+@property (weak, nonatomic) IBOutlet UITableView *table;
 
 @end
 
 @implementation ViewController
+- (IBAction)toggleEdit:(id)sender {
+    self.table.editing = !self.table.editing;
+}
+
+// 각 스타일을 번갈아 가면서 사용
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row % 3 == 0) {
+        return UITableViewCellEditingStyleNone;
+    }
+    else if (indexPath.row % 3 == 1) {
+        return UITableViewCellEditingStyleDelete;
+    }
+    else {
+        return UITableViewCellEditingStyleInsert;
+    }
+}
+
+// 삭제/추가 작업 - 로그로 확인
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (UITableViewCellEditingStyleDelete == editingStyle) {
+        NSLog(@"%d 번째 삭제", (int)indexPath.row);
+    }
+    else {
+        NSLog(@"%d 번째 추가", (int)indexPath.row);
+    }
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Dynamic Prototypes 방식 사용
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CELL_ID" forIndexPath:indexPath];
+    cell.textLabel.text = [NSString stringWithFormat:@"Cell %d", (int)indexPath.row];
+    return cell;
+}
 
 - (void)viewDidLoad
 {
